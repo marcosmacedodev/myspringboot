426a05676fea001af936baa7a3e83948b5a6e233
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-04-10 19:37:38 -0300

    Testando a primeira classe Model

--- src/main/java/com/myspringboot/controller/CategoriaController.java
+++ src/main/java/com/myspringboot/controller/CategoriaController.java
@@ -1,16 +1,26 @@
 package com.myspringboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.myspringboot.model.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaController {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST OK";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 	
 }
