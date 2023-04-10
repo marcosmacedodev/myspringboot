59e8c777283fba00cc5d02ecfc4eebefb17498a2
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-04-10 18:09:41 -0300

    Testando o Rest

--- src/main/java/com/myspringboot/MyspringbootApplication.java
+++ src/main/java/com/myspringboot/MyspringbootApplication.java
@@ -1,4 +1,4 @@
-package com.myspringboot.example;
+package com.myspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
