ddc2552e7a6230770836429a00d9410e2c19a3fe
Marcos Macedo <ricardo.paiva.macedo@hotmail.com>
2023-04-15 21:15:27 -0300

    Validação personalizada de e-mail para não repetição

--- src/main/java/com/myspringboot/repositories/ClienteRepository.java
+++ src/main/java/com/myspringboot/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.myspringboot.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.myspringboot.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/myspringboot/services/validation/InsertValidator.java
+++ src/main/java/com/myspringboot/services/validation/InsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.myspringboot.controller.exception.FieldMessage;
+import com.myspringboot.model.Cliente;
 import com.myspringboot.model.dto.ClienteNewDTO;
 import com.myspringboot.model.enums.TipoCliente;
+import com.myspringboot.repositories.ClienteRepository;
 import com.myspringboot.services.validation.utils.Utils;
 
 public class InsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 	 
+	@Autowired
+	private ClienteRepository cr;
+	
 	@Override
 	 public void initialize(ClienteInsert ann) {
 	 }
@@ -26,6 +33,12 @@ public class InsertValidator implements ConstraintValidator<ClienteInsert, Clien
 	 
 	 if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getId()) && !Utils.validateCNPJ(objDto.getCpfouCnpJ())) {
 		 list.add(new FieldMessage("cpfouCnpJ", "CNPJ inválido"));
+	 }
+	 
+	 Cliente obj = cr.findByEmail(objDto.getEmail());
+	 
+	 if (obj != null) {
+		 list.add(new FieldMessage("email", "Este e-mail já foi cadastrado. Tente outro e-mail."));
 	 }
 	 
 	 // inclua os testes aqui, inserindo erros na lista
